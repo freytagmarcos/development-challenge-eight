f9b5a69f4001e16902a2832c40b00ac982fe64b5
Marcos <marcosfreytag@gmail.com>
2022-11-19 10:23:13 -0300

    Changed DB connection

--- db/db.go
+++ db/db.go
@@ -4,10 +4,21 @@ import (
 	"database/sql"
 
 	_ "github.com/lib/pq"
+
+	"fmt"
+	"os"
+)
+
+var (
+	host     = os.Getenv("host")
+	user     = os.Getenv("user")
+	dbname   = os.Getenv("dbname")
+	password = os.Getenv("password")
+	port     = 5432
 )
 
 func ConectaComBancoDeDados() *sql.DB {
-	conexao := "user=postgres dbname=loja_online password=12345678 host=172.17.0.2 sslmode=disable"
+	conexao := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable", host, port, user, dbname, password)
 	db, err := sql.Open("postgres", conexao)
 
 	if err != nil {
